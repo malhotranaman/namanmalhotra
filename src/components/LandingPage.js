1dfa569edc2cb86efc4391302218ae8a15551773
Naman Malhotra <123405495+malhotranaman@users.noreply.github.com>
2024-08-30 17:39:36 +0100

    Create LandingPage.js

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+    return (
+        <main className="landing">
+            <h2>Welcome to My Personal Website!</h2>
+            <p>This is the landing page for Naman Malhotra's personal website.</p>
+        </main>
+    );
+}
+
+export default LandingPage;
